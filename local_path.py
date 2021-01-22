92e76d88f4a10b44603a00febeb7809868a2e9a4
xiaozai <ysongonly@gmail.com>
2021-01-22 20:01:58 +0200

    update local_path

--- local_path.py
+++ local_path.py
@@ -1,13 +1,17 @@
 # Dataset
 
 # LaSOT
-lasot_dir = '/home/dkn/daikenan/dataset/LASOT/LaSOTBenchmark/'
+lasot_dir = '/home/yans/Datasets/LaSOTBenchmark/'
 # OTB
-otb_dir = '/home/dkn/daikenan/dataset/OTB'
+otb_dir = '/home/yans/Datasets/OTB'
 # VOTLT2019
 votlt19_dir = '/home/yans/Datasets/votlt2019'
 # VOTLT2018
 votlt18_dir = '/home/daikenan/dataset/VOT18_long'
 # TLP
-tlp_dir = '/media/dkn/Data2/TLP'
+tlp_dir = '/home/yans/Datasets/TLP'
 base_path = '/media/dkn/Data2/LTMU'
+
+cdtb_depth_dir = ''
+cdtb_color_dir = ''
+cdtb_rgbd_dir = ['', '']
